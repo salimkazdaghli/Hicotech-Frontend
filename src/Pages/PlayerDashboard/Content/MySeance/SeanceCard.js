ad6ac6fdab083c318c29f148334f33d177b37853
salimkazdaghli <salimkazdaghli@gmail.com>
2022-04-09 15:31:24 +0100

    get all seance

---
+++ src/Pages/PlayerDashboard/Content/MySeance/SeanceCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Skeleton, Card, Avatar, Col } from "antd";
+
+const SeanceCard = (props) => {
+  const { Meta } = Card;
+  const { seance, loading } = props;
+  return (
+    <Col span={8}>
+      <Card style={{ marginTop: 16 }} actions={[]}>
+        <Skeleton loading={!loading} avatar active>
+          <Meta
+            avatar={
+              <Avatar style={{ color: "#fff", backgroundColor: "#36a2e1" }}>
+                A
+              </Avatar>
+            }
+            title={seance.seanceName}
+          />
+          <p> DateSeance : {seance.dateSeance} </p>
+        </Skeleton>
+      </Card>
+    </Col>
+  );
+};
+export default SeanceCard;
