f21980c564044c65988c4ca76692a0dc96e30808
github_MzOumaima <mzoughi.oumaimaa@gmail.com>
2022-04-24 00:50:52 +0200

    update event en tq joueur

---
+++ src/Pages/PlayerDashboard/Content/event/Events.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { Row, Spin, Space, Col, Typography } from "antd";
+import { getAllEventApi } from "../../../../Services/EventService";
+
+import EventCard from "./EventCard";
+
+import Event from "./Event";
+
+const { Title } = Typography;
+
+const Events = () => {
+  const [loading, setLoading] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [eventSelected, setEventSelected] = useState({});
+  const [isEventVisible, setEventVisible] = useState(false);
+
+  async function getEvents() {
+    await getAllEventApi({ eventVisible: true })
+      .then((response) => {
+        setEvents(response.data);
+        setLoading(true);
+      })
+      .catch(() => {});
+  }
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  return (
+    <>
+      <Title>Mes évenements </Title>
+      <div className="site-card-wrapper">
+        {loading && (
+          <Row gutter={10}>
+            {events.map((event) => (
+              <EventCard
+                event={event}
+                loading={loading}
+                key={event._id}
+                events={events}
+                setEvents={setEvents}
+                setLoading={setLoading}
+                setIsModalVisible={setIsModalVisible}
+                isModalVisible={isModalVisible}
+                setEventSelected={setEventSelected}
+                setEventVisible={setEventVisible}
+              />
+            ))}
+          </Row>
+        )}
+        {!loading && (
+          <Row gutter={16}>
+            <Col span={8}>
+              <Space size="middle" style={{ marginTop: 250, marginLeft: 600 }}>
+                <Spin size="large" tip="Loading..." />
+              </Space>
+            </Col>
+          </Row>
+        )}
+      </div>
+      {isEventVisible && (
+        <Event
+          isEventVisible={isEventVisible}
+          event={eventSelected}
+          setEventVisible={setEventVisible}
+        />
+      )}
+    </>
+  );
+};
+export default Events;
--- src/Pages/PlayerDashboard/Routes/DashboardRouting.js
+++ src/Pages/PlayerDashboard/Routes/DashboardRouting.js
@@ -2,6 +2,7 @@ import { Switch } from "react-router-dom";
 import MyChellenges from "../Content/MyChallenges";
 import JoueurRoute from "../../../Routes/joueurRoute";
 import Seances from "../Content/MySeance/Seances";
+import Events from "../Content/event/Events";
 
 const DashboardRouting = () => (
   <Switch>
@@ -11,6 +12,7 @@ const DashboardRouting = () => (
       component={MyChellenges}
     />
     <JoueurRoute exact path="/joueur/dashboard/seances" component={Seances} />
+    <JoueurRoute exact path="/joueur/dashboard/mesevents" component={Events} />
   </Switch>
 );
 
